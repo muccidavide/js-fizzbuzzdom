15c087aae00a9922d7964ec811b6e7036cdd7278
muccidavide <davidemucci0804@gmail.com>
2022-03-28 16:27:59 +0200

    Complite first Bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,80 @@ CONSIGLI:
 scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 #endregion */
+
+// stampare in console numeri da 1 a 100
+const numberElement = document.querySelector('.number_box')
+
+for ( let i = 1 ; i <= 100; i++){
+   
+
+/*     if (i % 3 === 0 && i % 5 === 0) {  // se sia divisibile per 3 che per 5
+        console.log('FizzBuzz');
+
+    } else if (i % 3 === 0){  //  divisibile per 3 
+        console.log('Fizz');
+    }
+    else if (i % 5 === 0){  //  divisibile per 5 
+        console.log('Buzz');
+    }
+
+    else{
+        console.log(i); // tutti gli altri numeri
+    }
+ */
+
+    if (i % 3 === 0 && i % 5 === 0) {  // se sia divisibile per 3 che per 5
+        console.log('FizzBuzz');
+
+        const markupNumber = `
+        <div class="number ">
+        <div>FizzBuzz</div>
+        </div>`
+    
+        numberElement.insertAdjacentHTML('beforeend', markupNumber)
+
+    } else if (i % 3 === 0){  //  divisibile per 3 
+        console.log('Fizz');
+
+        const markupNumber = `
+        <div class="number">
+        <div>Fizz</div>
+        </div>`
+    
+        numberElement.insertAdjacentHTML('beforeend', markupNumber)
+    }
+    else if (i % 5 === 0){  //  divisibile per 5 
+        console.log('Buzz');
+
+        const markupNumber = `
+        <div class="number">
+        <div>Buzz</div>
+        </div>`
+    
+        numberElement.insertAdjacentHTML('beforeend', markupNumber)
+    }
+
+    else{
+        console.log(i); // tutti gli altri numeri
+
+        const markupNumber = `
+        <div class="number">
+        <div>${i}</div>
+        </div>`
+
+        numberElement.insertAdjacentHTML('beforeend', markupNumber)
+    }
+
+
+
+
+
+
+
+   
+
+
+
+
+
+}
