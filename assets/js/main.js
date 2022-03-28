017b3d5fb3021286fc69bddb17df33b49ae860df
muccidavide <davidemucci0804@gmail.com>
2022-03-28 16:49:04 +0200

    Complete with Bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,22 +23,6 @@ proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro program
 const numberElement = document.querySelector('.number_box')
 
 for ( let i = 1 ; i <= 100; i++){
-   
-
-/*     if (i % 3 === 0 && i % 5 === 0) {  // se sia divisibile per 3 che per 5
-        console.log('FizzBuzz');
-
-    } else if (i % 3 === 0){  //  divisibile per 3 
-        console.log('Fizz');
-    }
-    else if (i % 5 === 0){  //  divisibile per 5 
-        console.log('Buzz');
-    }
-
-    else{
-        console.log(i); // tutti gli altri numeri
-    }
- */
 
     if (i % 3 === 0 && i % 5 === 0) {  // se sia divisibile per 3 che per 5
         console.log('FizzBuzz');
